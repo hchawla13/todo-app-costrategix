cacb3b3e9532b7e56842db8017b98837923474d2
Harpreet Singh Chawla <hchawla@lowes.com>
2021-03-30 17:04:20 +0530

    removing errors

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,22 +1,9 @@
-import AddToDo from './AddToDo';
-import ShowList from './ShowList';
-import { useState } from 'react';
 const ToDo = () => {
-    const [parentList, setParentList ] = useState(JSON.parse(localStorage.getItem('todoList')));
-    const getList = (param) => {
-        console.log("parentList",param);
-        setParentList(param);
-    }
     return (
-        <>
-            <div>
-                {/* <ShowList myList={parentList}></ShowList> */}
-                
-            </div>
-            {/* <div><AddToDo getToDoListInParent={getList}></AddToDo></div> */}
+        <div>
             This is the basic app to be deployed
             Purpose is to check for sso
-        </>
+        </div>
     )
 }
 export default ToDo;
