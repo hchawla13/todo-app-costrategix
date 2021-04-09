e70eb0245f1f22a057e76d1668dfe2f0b5a66857
Harpreet Singh Chawla <hchawla@lowes.com>
2021-04-09 22:21:23 +0530

    entire stringify

--- src/components/TestApi.js
+++ src/components/TestApi.js
@@ -1,13 +1,14 @@
 import {useEffect} from 'react';
 import axios from 'axios';
+import qs from 'querystring';
 const TestApi = () => {
     const revokeTokenEndpoint = 'https://qalius.myloweslife.com/affwebservices/CASSO/oidc/csg/revoke'
     const revokeToken = () => {
-        const data = {
+        const data = qs.stringify({
             token: 'MGNjMzMyYTUtMDdiYy00OGY4LTgzMTAtNmU0MDYzY2UxZDUwLUp2b2JvcHVYY1RTS0VoOU85TDBURWVSbzRPdz0=',
             client_id: '00053f8d-a5a5-106d-8039-a0cfac1d0000',
             token_type_hint: 'access_token'
-        };
+        });
         const promise = fetch(revokeTokenEndpoint, {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
